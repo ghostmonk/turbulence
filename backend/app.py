1a71e71c9a8d738307093750b9592ec43e2dd2a7
Nicholas Hillier <nicholas.hillier@ro.co>
2024-11-30 08:30:57 -0500

    updated site to use mongo free tier

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,15 @@ from database import get_db
 
 app = Flask(__name__)
 
+# Connect to the database
+db = get_db()
+collection = db["flexible_content"]
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM flexible_content;")
-    rows = cursor.fetchall()
-    return jsonify([{"id": row[0], "content": row[1]} for row in rows])
+    # Retrieve all documents from the MongoDB collection
+    data = list(collection.find({}, {"_id": 0}))  # Exclude MongoDB's _id field
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,24 @@
-import psycopg2
+from pymongo import MongoClient
 import os
 
 def get_db():
-    conn = psycopg2.connect(
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host="/cloudsql/" + os.getenv("CLOUD_SQL_CONNECTION_NAME")
-    )
-    return conn
+    # MongoDB connection URI
+    user = _get_variable("MONGO_USER")
+    password = _get_variable("MONGO_PASSWORD")
+    cluster = _get_variable("MONGO_CLUSTER")
+    app_name = _get_variable("MONGO_APP_NAME")
+    host = _get_variable("MONGO_APP_NAME")
+    if not password:
+        raise ValueError("MONGO_PASSWORD environment variable is not set")
+
+    mongo_uri = f"mongodb+srv://{user}:{password}@{cluster}.{host}/?retryWrites=true&w=majority&appName={app_name}"
+    client = MongoClient(mongo_uri)
+    db_name = os.getenv("MONGO_DB_NAME", "ghostmonk")
+
+    return client[db_name]
+
+def _get_variable(key: str) -> str:
+    output = os.getenv(key)
+    if not output:
+        raise ValueError(f"{key} environment variable is not set")
+    return output
